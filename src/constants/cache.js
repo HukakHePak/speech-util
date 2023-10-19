5eb0bbb2fd13b377b5384777f6d546c43ecdb232
HukakHePak <valsidalvonilb@outlook.com>
2023-10-19 18:16:17 +0400

    fix: cache require

--- src/constants/cache.js
+++ src/constants/cache.js
@@ -2,3 +2,5 @@ const CACHE = {
     ENABLED: true,
     TIME: 60 * 24 * 30 * 2 // 2 months
 }
+
+module.exports = CACHE
--- src/routes/speech.js
+++ src/routes/speech.js
@@ -1,11 +1,13 @@
+const CACHE = require("../constants/cache");
+
 function speechRoute(lang) {
   const speech = require("node-gtts")(lang);
   const router = require("express").Router();
 
   router.get("/", function (req, res) {
     res.set({ "Content-Type": "audio/mpeg" });
-    
-    if(CACHE.ENABLED) {
+
+    if (CACHE.ENABLED) {
       res.set("Cache-control", `public, max-age=${CACHE.TIME}`);
     }
 
